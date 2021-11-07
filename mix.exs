32978e5d1c805e3d03061d8ebc9b7121836e6245
Dirk Elmendorf <dirk@r26d.com>
2021-11-07 07:52:27 -0600

    updated the version and the git hub link

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Towel.Mixfile do
   def project do
     [app: :towel,
      description: "A delightfully simple monad library that's written for Elixir.",
-     version: "0.2.2",
+     version: "0.3.2",
      elixir: "~> 1.0",
      package: package(),
      build_embedded: Mix.env == :prod,
@@ -14,8 +14,8 @@ defmodule Towel.Mixfile do
 
   def package do
     [licenses: ["MIT"],
-     contributors: ["Kash Nouroozi"],
-     links: %{"Github" => "https://github.com/knrz/towel"}]
+     contributors: ["Kash Nouroozi","tim-smart"],
+     links: %{"Github" => "https://github.com/r26D/towel"}]
   end
 
   def application do
